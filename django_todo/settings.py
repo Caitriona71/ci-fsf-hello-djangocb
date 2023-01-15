c7283f84179abdc888fdf0f4a44f7d8f77f2feb5
Caitriona71 <caitrionaboyle@ymail.com>
2023-01-15 12:35:51 +0000

    prepared again to deploy to Heroku

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -20,7 +20,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 import os
-if os.path.exists('env.py'):
+if os.path.isfile('env.py'):
     import env
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
@@ -30,9 +30,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j82i2(06libeg@ct8_q5g-az(m$)bj01w$*5n*x^2^(s1#pyx%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
