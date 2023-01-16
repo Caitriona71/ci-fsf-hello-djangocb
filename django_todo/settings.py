e6fe94baa60d67d436dfac05fc4df8af21d773e3
Caitriona71 <caitrionaboyle@ymail.com>
2023-01-16 11:00:15 +0000

    Fixed allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ckz8780-django-todo-appcb.herokuapp.com']
 
 
 # Application definition
